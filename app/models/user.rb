22c898502b6c0ebcc4ac17598c8e33aa8552fae4
yasin <yasin.elsharawi23@gmail.com>
2022-09-07 14:11:58 +0300

    Setup authorization and files Install CanCanCan Implement the splash page

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  def admin?(requested_role)
+    role == requested_role.to_s
+  end
 end
