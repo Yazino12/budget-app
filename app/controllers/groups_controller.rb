a3d44376d1139c008e395a91bf9c7ca2865b7dee
yasin <yasin.elsharawi23@gmail.com>
2022-09-10 09:59:42 +0300

    Add requested changes Update categories icon selection to avoid user generated bugs

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -14,10 +14,28 @@ class GroupsController < ApplicationController
 
   def new
     @group = Group.new
+    @icons = ['health','sneakers','food','electronics','cars','furniture']
   end
 
   def create
     @group = current_user.groups.new(group_params)
+
+    case @group.icon 
+    when 'Health'
+      @group.icon = 'https://cdn-icons-png.flaticon.com/512/2382/2382461.png'
+    when 'Sneakers' 
+      @group.icon = 'https://cdn-icons-png.flaticon.com/512/2741/2741289.png'
+    when 'Food' 
+      @group.icon = 'https://cdn-icons-png.flaticon.com/512/2276/2276931.png'
+    when 'Electronics' 
+      @group.icon = 'https://cdn-icons-png.flaticon.com/512/1261/1261106.png'
+    when 'Cars' 
+      @group.icon = 'https://cdn-icons-png.flaticon.com/512/2330/2330453.png'
+    when 'Furniture' 
+      @group.icon = 'https://cdn-icons-png.flaticon.com/512/2590/2590525.png'
+    else
+      @group.icon = 'https://cdn-icons-png.flaticon.com/512/7734/7734301.png'
+    end
 
     if @group.save
       redirect_to groups_path, flash: { success: 'Category was successfully created.' }
